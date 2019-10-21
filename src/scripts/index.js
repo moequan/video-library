c57bc7f26afa6d2a830cfcffc7d9c2aea0630685
moequan <maurice.s93@web.de>
2019-10-21 11:14:45 +0200

    Starting with simple structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,6 @@
 // Any code you will write for your website (in the future!) should go here
+
+showPlayer = e => {
+  let player = document.querySelector(".player");
+  player.classList.add("active");
+};
